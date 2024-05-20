1161127b34c3ec8b1721463c88efd74343e3dae3
chr1st1ank <chr1st1ank@krudewig-online.de>
2024-05-20 13:25:38 +0200

    Allow to set temperature

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # vitonubes
+
+Home Assistant integration setup for a Vitocal 252 A heatpump.
+
+Based on the following projects:
+
+- https://github.com/open3e/open3e
+- https://github.com/Wolfgang-03/Open3E-HA
+
+## Installation
+
+The installation was tested on a [Raspberry Pi Zero 2 W](https://www.raspberrypi.com/products/raspberry-pi-zero-2-w/) with a [Canable USB to CAN Bus adapter](https://canable.io/).
+
+- Set up a default raspbian operating system.
+- Clone this repository to the raspberry pi.
+- Run the [scripts](scripts/) in order. Watch out for error messages and report them as issue here, if necessary.
+- For the home assistant integration follow the [instructions by @Wolfgang-03](https://github.com/Wolfgang-03/Open3E-HA/blob/main/installation.md#device-und-entities-in-home-assistant-bereitstellen)
--- start.sh
+++ start.sh
@@ -11,12 +11,8 @@ source .env || echo "Export the necessary environment variables in .env!"
 --timestep 30 \
 --mqtt $MQTT_HOST:${MQTT_PORT:-1883}:${MQTT_TOPIC:-open3e} \
 --mqttuser $MQTT_USER:$MQTT_PASSWORD \
---mqttformatstring {ecuAddr:03X}_{didNumber}_{didName}
-# --listen open3e/cmnd
+--mqttformatstring {ecuAddr:03X}_{didNumber}_{didName} \
+--listen ${MQTT_TOPIC:-open3e}/cmnd \
+--verbose 
 # --config open3e_config/Open3Edatapoints.json \
 #--config devices.json
-#  --verbose 
-
-# Including mixerTwo:
-# --read 268,269,271,274,284,286,318,320,321,322,324,325,355,381,389,391,396,401,402,424,426,475,476,543,880,881,901,902,987,988,1043,1100,1101,1102,1103,1415,1416,1643,1644,1731,1769,1770,1771,1772,1775,1776,2256,2333,2334,2346,2350,2351,2352,2369,2370,2487,2488,2496,2543,2544,2545,2560,2569,2735,2791,2792,2793,2855,2856,3016,0x68c.2256,0x6cf.2442
- 
